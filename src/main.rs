ffed9dfbfbd74001b3cef13f53d4956d3755d90e
Aksh Bansal <akshmark@gmail.com>
2021-09-09 15:39:35 +0530

    feat: recursive search through all files

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod utils;
 struct Arg{
     pattern: String,
     path: String,
-    ignore_case: bool
+    ignore_case: bool,
+    is_dir: bool
 }
 
 fn main() {
@@ -11,30 +12,66 @@ fn main() {
     let mut arg: Arg = Arg{
         pattern: std::env::args().nth(len-2).expect("no pattern given"),
         path: std::env::args().nth(len-1).expect("no path given"),
-        ignore_case: false
+        ignore_case: false,
+        is_dir: false
     };
 
     for i in 1..len{
         let cur = std::env::args().nth(i).unwrap();
         match cur.as_str() {
             "-i" | "--ignore-case" => arg.ignore_case = true,
+            "-d" | "--dir" => arg.is_dir = true,
             "-h" | "--help" => { utils::help::print_help(); return},
             _ => ()
         }
     }
 
-    // Searching single file
+    // Execution Time start
     let time_start = std::time::SystemTime::now();
 
-    let contents = std::fs::read_to_string(arg.path).expect("Invalid path");
-    let arr: Vec<&str> = contents.split('\n').collect();
-
-    for i in 0..arr.len(){
-        if utils::is_present(&arg.pattern, arr[i], arg.ignore_case) {
-            print!("[{}] {}\n", i+1, arr[i]);
-        }
+    if arg.is_dir{
+        visit_dirs(&arg.path, &arg).expect("Invalid path");
+    }
+    else{
+        search_file(&arg.path, &arg);
     }
 
     print!("\nExecuted in {}ms\n", time_start.elapsed().unwrap().as_millis());
 }
 
+fn visit_dirs(dir: &str, arg:&Arg) -> std::io::Result<()> {
+    for entry in std::fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            visit_dirs(&path.to_str().unwrap(), arg)?;
+        } else {
+            search_file(&path.to_str().unwrap(), arg);
+        }
+    }
+    Ok(())
+}
+
+fn search_file(path:&str, arg:&Arg){
+    let contents = match std::fs::read_to_string(path){
+        Ok(v)=>v,
+        _ => return
+    };
+    let arr: Vec<&str> = contents.split('\n').collect();
+    let mut res = String::new();
+    let mut check = false;
+
+    for i in 0..arr.len(){
+        if utils::is_present(&arg.pattern, arr[i], arg.ignore_case) {
+            check = true;
+            res.push_str(&format!("[{}] {}\n", i+1, arr[i]));
+        }
+    }
+    if check {
+        print!("\n[{}]\n{}",get_filepath(path), res) ;
+    }
+}
+
+fn get_filepath(path:&str)->&str{
+    path
+}
